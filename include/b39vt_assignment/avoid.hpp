965e39813eeaa9a981dcb644c89dc6202d9c59a8
Grant <gm12@hw.ac.uk>
2017-02-27 11:24:46 +0000

    basic avoidance

---
+++ include/b39vt_assignment/avoid.hpp
@@ -0,0 +1,44 @@
+#ifndef AVOID_B39VT_ASSIGNMENT_HPP
+#define AVOID_B39VT_ASSIGNMENT_HPP
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <ros/ros.h>
+#include <sensor_msgs/image_encodings.h>
+#include "tf/transform_listener.h"
+#include "sensor_msgs/PointCloud.h"
+#include "tf/message_filter.h"
+#include "message_filters/subscriber.h"
+#include "laser_geometry/laser_geometry.h"
+#include "geometry_msgs/Twist.h"
+
+class LaserSubscriber
+{
+	// By default, variables and methods are private
+	ros::Subscriber sub_;
+ 	ros::NodeHandle n_;
+  laser_geometry::LaserProjection projector_;
+  tf::TransformListener listener_;
+  message_filters::Subscriber<sensor_msgs::LaserScan> laser_sub_;
+  tf::MessageFilter<sensor_msgs::LaserScan> laser_notifier_;
+  ros::Publisher scan_pub_;
+  ros::Publisher vel_pub_;
+ 
+public:
+	// Public variables - it is a good practice to separate the declaration of
+	// variables from the declaration/definition of methods
+	bool data_valid;
+  
+public:
+	// Public methods
+  LaserSubscriber(ros::NodeHandle n);
+  ~LaserSubscriber();
+  
+  void laserCb(const sensor_msgs::LaserScan& msg);
+  void publish(const sensor_msgs::LaserScan& msg);
+};
+// Your code goes here
+
+# endif // AVOID_B39VT_ASSIGNMENT_HPP
---
+++ src/avoidance.cpp
@@ -0,0 +1,92 @@
+#include <ros/ros.h>
+#include <sensor_msgs/image_encodings.h>
+#include "tf/transform_listener.h"
+#include "sensor_msgs/PointCloud.h"
+#include "tf/message_filter.h"
+#include "message_filters/subscriber.h"
+#include "laser_geometry/laser_geometry.h"
+#include "geometry_msgs/Twist.h"
+
+#include "b39vt_assignment/avoid.hpp"
+
+LaserSubscriber::LaserSubscriber(ros::NodeHandle n) : n_(n), 
+	data_valid(false), laser_sub_(n_, "base_scan", 10),
+	laser_notifier_(laser_sub_,listener_, "base_link", 10){
+  	sub_ = n_.subscribe("scan", 1000, &LaserSubscriber::laserCb, this);
+  	scan_pub_ = n_.advertise<sensor_msgs::PointCloud>("/my_cloud",1);
+  	vel_pub_ = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 3); 
+}
+
+LaserSubscriber::~LaserSubscriber() {  }
+  
+
+void LaserSubscriber::laserCb(const sensor_msgs::LaserScan& msg)
+{
+    sensor_msgs::PointCloud cloud;
+    
+    ROS_INFO("In callback function");
+    try
+    {
+        ROS_INFO("Within try catch");
+        //projector_.transformLaserScanToPointCloud("base_link", msg, cloud, listener_);
+        publish(msg);
+    }
+    catch (tf::TransformException& e)
+    {
+        std::cout << e.what();
+        return;
+    }
+}
+   
+void LaserSubscriber::publish(const sensor_msgs::LaserScan& msg){ 
+   
+	geometry_msgs::Twist vel;
+   
+	vel.linear.x = 0.25;
+	vel.linear.y = 0;
+	vel.linear.z = 0;
+	vel.angular.x = 0;
+	vel.angular.y = 0;
+	vel.angular.z = 0;
+	
+	ROS_INFO("Publishing message");
+		
+  
+  //ROS_INFO("Min Value: ");
+  //ROS_INFO(min_element(msg.ranges,msg.ranges+len);
+  float min = 10000;
+  float max = 0;
+  for (int i = 0;i<msg.ranges.size();i++){
+  if(msg.ranges[i] < min && msg.ranges[i]>0.15){min = msg.ranges[i];}
+  if(msg.ranges[i] > max){max = msg.ranges[i];}
+  }
+  ROS_WARN_STREAM("Min value :"<< min);
+  ROS_WARN_STREAM("Max value :"<< max);
+  
+  vel_pub_.publish(vel);
+  if( min< 0.5 && min > 0.15){
+  vel.linear.x = 0;
+  vel_pub_.publish(vel);
+  
+  
+  vel.angular.z = 0.5;
+  vel_pub_.publish(vel);
+  
+  };
+  
+  
+}
+  
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "avoid");
+  ros::NodeHandle n;
+  
+  LaserSubscriber ls(n);
+	//LaserScanToPointCloud lstopc(n);
+  
+  ros::spin();
+  
+  return 0;
+}
