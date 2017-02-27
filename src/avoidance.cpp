719edf09496c8351feb1c72eeaae278a1dca76ab
Grant <gm12@hw.ac.uk>
2017-02-27 15:52:12 +0000

    avoid, left, right, both

--- src/avoidance.cpp
+++ src/avoidance.cpp
@@ -9,112 +9,114 @@
 
 #include "b39vt_assignment/avoid.hpp"
 
-LaserSubscriber::LaserSubscriber(ros::NodeHandle n) : n_(n), 
-	data_valid(false), laser_sub_(n_, "base_scan", 10),
-	laser_notifier_(laser_sub_,listener_, "base_link", 10){
-  	sub_ = n_.subscribe("scan", 1000, &LaserSubscriber::laserCb, this);
-  	scan_pub_ = n_.advertise<sensor_msgs::PointCloud>("/my_cloud",1);
-  	vel_pub_ = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 3); 
+//subscribes to laser
+LaserSubscriber::LaserSubscriber(ros::NodeHandle n)
+    : n_(n)
+    , data_valid(false)
+    , laser_sub_(n_, "base_scan", 10)
+    , laser_notifier_(laser_sub_, listener_, "base_link", 10)
+{
+    sub_ = n_.subscribe("scan", 1000, &LaserSubscriber::laserCb, this);
+    vel_pub_ = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 3); //publishes velocity commands to base
 }
 
-LaserSubscriber::~LaserSubscriber() {  }
-  
+LaserSubscriber::~LaserSubscriber() {}
 
 void LaserSubscriber::laserCb(const sensor_msgs::LaserScan& msg)
 {
-    sensor_msgs::PointCloud cloud;
-    
-    ROS_INFO("In callback function");
-    try
-    {
-        ROS_INFO("Within try catch");
-        //projector_.transformLaserScanToPointCloud("base_link", msg, cloud, listener_);
+    try {
         publish(msg);
     }
-    catch (tf::TransformException& e)
-    {
+    catch (tf::TransformException& e) {
         std::cout << e.what();
         return;
     }
 }
-   
-void LaserSubscriber::publish(const sensor_msgs::LaserScan& msg){ 
-   
-	geometry_msgs::Twist vel;
-   
-  float linearx = 0.25;
-	vel.linear.x = linearx;
-	vel.linear.y = 0;
-	vel.linear.z = 0;
-	vel.angular.x = 0;
-	vel.angular.y = 0;
-	vel.angular.z = 0;
-	
-	ROS_INFO("Publishing message");
 
-  float min = 10000;
-  float max = 0;
-  for (int i = 127.75;i<(msg.ranges.size()-127.75);i++){
-  if(msg.ranges[i] < min && msg.ranges[i]>0.15){min = msg.ranges[i];}
-  if(msg.ranges[i] > max){max = msg.ranges[i];}
-  }
-  
-  
-  ROS_WARN_STREAM("Min value :"<< min);
-  ROS_WARN_STREAM("Max value :"<< max);
-  
-  vel_pub_.publish(vel);
-  if( min< 0.5 && min > 0.15){
-  vel.linear.x = 0;
-  vel_pub_.publish(vel);
-  
-  float minr = 10000;
-  float minl = 10000;
-  //right side
-  for (int i = 383.25;i<(msg.ranges.size());i++){
-  if(msg.ranges[i] < minr && msg.ranges[i]>0.15){minr = msg.ranges[i];}
-  }
-  //left side
-  for (int i = 0;i<(msg.ranges.size()-383.25);i++){
-  if(msg.ranges[i] < minl && msg.ranges[i]>0.15){minl = msg.ranges[i];}
-  }
-  float angularv = 0;
-  
-  //both
-  if(minr<0.5 && minl<0.5){
-  
-  ros::Time start2 = ros::Time::now();
-	while(ros::Time::now() - start2 < ros::Duration(0.2))
-	{
-	vel.linear.x = linearx/-2;
-	vel_pub_.publish(vel);
-	}
-  if(minl > minr){angularv = -0.9;}
-  else {angularv = 0.9;}
-  } 
-  
-  if(minl > minr){angularv = -0.9;}
-  else {angularv = 0.9;}
-  
-  
-  vel.angular.z = angularv;
-  vel_pub_.publish(vel);
-  
-  };
-  
-  
+void LaserSubscriber::publish(const sensor_msgs::LaserScan& msg)
+{
+
+    geometry_msgs::Twist vel;
+		
+		//creates initial velocity (no obstacles)
+    float linearx = 0.25;
+    vel.linear.x = linearx;
+    vel.linear.y = 0;
+    vel.linear.z = 0;
+    vel.angular.x = 0;
+    vel.angular.y = 0;
+    vel.angular.z = 0;
+
+		//looks at the central 60 degree (30 each of centre)
+    float min = 10000;
+    for (int i = 170.75; i < (msg.ranges.size() - 170.75); i++) {
+        if (msg.ranges[i] < min && msg.ranges[i] > 0.15) {
+            min = msg.ranges[i];
+        }
+    }
+
+    vel_pub_.publish(vel);
+    if (min < 0.5 && min > 0.15) {
+        vel.linear.x = 0;
+        vel_pub_.publish(vel);
+
+        float minr = 10000;
+        float minl = 10000;
+        
+        //looks at right side (60 degrees) - turns left if obstacle on right
+        for (int i = 340.25; i < (msg.ranges.size()); i++) {
+            if (msg.ranges[i] < minr && msg.ranges[i] > 0.15) {
+                minr = msg.ranges[i];
+            }
+        }
+        
+        //looks at left side (60 degree) - turns right if obstacle no right
+        for (int i = 0; i < (msg.ranges.size() - 340.25); i++) {
+            if (msg.ranges[i] < minl && msg.ranges[i] > 0.15) {
+                minl = msg.ranges[i];
+            }
+        }
+        float angularv = 0;
+
+        //incase obstacales are infront and both sides - reverses, then rescans area
+        if (minr < 0.25 && minl < 0.25 && min < 0.5) {
+
+            ros::Time start2 = ros::Time::now();
+            while (ros::Time::now() - start2 < ros::Duration(0.3)) { //reverses for 0.3 time increments
+                vel.linear.x = linearx / -2; //sets reverse speed
+                vel_pub_.publish(vel); 
+            }
+
+						//once reversed, rechecks left and right hand side to determine which way to progress
+            if (minl > minr) {
+                angularv = -0.9;
+            }
+            else {
+                angularv = 0.9;
+            }
+        }
+
+        if (minl > minr) {
+            angularv = -0.9;
+        }
+        else {
+            angularv = 0.9;
+        }
+
+        vel.angular.z = angularv;
+        vel_pub_.publish(vel);
+        vel.angular.z = 0;
+    };
 }
-  
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "avoid");
-  ros::NodeHandle n;
-  
-  LaserSubscriber ls(n);
-	//LaserScanToPointCloud lstopc(n);
-  
-  ros::spin();
-  
-  return 0;
+    ros::init(argc, argv, "avoid");
+    ros::NodeHandle n;
+
+    LaserSubscriber ls(n);
+
+    ros::spin();
+
+    return 0;
 }
